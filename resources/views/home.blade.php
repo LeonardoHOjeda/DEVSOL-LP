5a491b8be3752c89ac39bafcd454eb5b3dde1790
Leonardo Ojeda <perros_rebeldes@hotmail.com>
2024-05-24 17:57:04 -0600

    feat: crear app layout file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Inicio</title>
+@extends('layouts.app')
 
-  @vite('resources/css/app.css')
-</head>
-<body>
-  <h1 class="text-red-500 underline">Bienvenido a la pagina principal</h1>
-</body>
-</html>
+@section('title', 'DevSolutions MX')
+
+@section('content')
+  <h1>Home</h1>
+  <p>Welcome to DevSolutions MX</p>
+@endsection
